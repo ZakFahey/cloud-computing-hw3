fa41b70268b8c345f38e79a28e59f45d17933b91
ZakFahey <z.fahey@gmail.com>
2019-03-18 19:50:16 -0400

    Added result from openweathermap.org

--- src/index.js
+++ src/index.js
@@ -1,28 +1,36 @@
-var host = 'http://localhost:8000';
+var host = 'http:localhost:8000';
 
 $('#submit').click(function (e) {
+    $('#error').text('');
     $.ajax(host + '/forecast/' + $('#date').val())
         .done(OnAPILoad)
         .fail(OnAPIFail);
+    // External API is fetched through our API so that openweathermap.org's API key isn't exposed.
+    $.ajax(host + '/forecast/external')
+        .done(OnExternalAPILoad);
 });
 
-function OnAPILoad(result) {
-    var html =
-        '<table>' +
+function getTableForTemps(temps) {
+    return '<table>' +
         '<thead>' +
         '<tr>' +
         '<th>Date</th><th>Max Temp</th><th>Min Temp</th>' +
         '</tr>' +
         '</thead>' +
         '<tbody>' +
-        result.map(function (r) {
-            return '<tr><th>' + r.DATE + '</th><th>' + r.TMAX + '</th><th>' + r.TMIN + '</th></tr>';
+        temps.map(function (t) {
+            return '<tr><th>' + t.DATE + '</th><th>' + t.TMAX + '</th><th>' + t.TMIN + '</th></tr>';
         }).join('') +
         '</tbody>' +
         '</table>';
+}
 
-    $('#results').html(html);
-    $('#error').text('');
+function OnAPILoad(result) {
+    $('#results').html('<h2>Weather forecast from my API</h2>' + getTableForTemps(result));
+}
+
+function OnExternalAPILoad(result) {
+    $('#results2').html('<h2>Weather forecast from openweathermap.org</h2>' + getTableForTemps(result));
 }
 
 function OnAPIFail(err) {
